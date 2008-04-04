fd7a8d48667865b712201496f8b5aff46cc545bc
Jim Van Fleet <bigfleet@administrators-computer.local>
2008-04-04 14:41:34 -0400

    Bringing in the needed Rails libraries to begin testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
-require File.dirname(__FILE__) + '/../lib/data_table'
+require 'rubygems'
+
+require File.join(File.dirname(__FILE__), 'boot') unless defined?(ActiveRecord)
+
+require 'data_table'
+
+# Wrap tests that use Mocha and skip if unavailable.
+def uses_mocha(test_name)
+  require 'mocha' unless Object.const_defined?(:Mocha)
+rescue LoadError => load_error
+  $stderr.puts "Skipping #{test_name} tests. `gem install mocha` and try again."
+else
+  yield
+end
