3ab61ac8204f57efcd0650e1aeaf0cb7d0828b1c
Jim Van Fleet <jim@jimvanfleet.com>
2008-05-29 15:26:44 -0400

    Getting ready to move to my other computer-- nothing to see here, move along

---
+++ lib/data_table/filter_view_helpers.rb
@@ -0,0 +1,74 @@
+module DataTable
+  module FilterViewHelpers
+    # filter_form takes the following family of options
+    # :id   => the ID of the form that contains the filtration selections
+    # :form => 
+    #    :url => If you desire a standard form submission, provide the url here
+    #    :method => The method to use when submitting the form (e.g. GET, POST) 
+    # :remote =>
+    #    :url => The remote URL to submit to when a filter is selected
+    #    :update => The div to update when the new data is available
+    #    :method => The method to use when submitting the form (e.g. GET, POST) 
+    #    :complete => The method to call when the remote connection is complete
+    # :html =>
+    #   :label => The introductory label of the filter form
+    #   :inner_frame => The options hash of HTML options you'd like the filters inner div to use
+    def filter_form(name=nil, options={})
+      remote_options = finalize_options(options)
+      filter = find_filter(name).with(params)
+      form_for_filter(filter, options)
+    end
+    
+
+    
+    protected
+    
+
+    
+    def finalize_options(options)
+      remote_options = { 
+        :url => options.delete(:url),
+        :update => options.delete(:update),
+        :method => :get
+      }
+      options.reverse_merge!({:id => 'filterForm'})
+    end
+    
+    def templates_detected?
+      # Should expand a RAILS_ROOT and render the needed 
+    end
+    
+    def form_for_filter(filter, options)
+      return unless filter
+      return "YAY" if templates_detected?
+      return form_for_filter_via_builder(filter, options)
+    end
+    
+    def form_for_filter_via_builder(filter, options)
+      xml = Builder::XmlMarkup.new
+      html_options = options[:html] || {}
+      form_options = (options[:form] || {}).reverse_merge({:url => ""})
+      remote_options = (options[:remote] || {}).reverse_merge({:url => "", :method => :get})
+      xml.label html_options[:label] if html_options[:label]
+      case filter.mode
+      when :ajax
+        xml << form_remote_tag(remote_options.merge(:id => options[:id]))
+      else
+        xml << form_tag(form_options[:url], form_options.merge(:id => options[:id]))
+      end
+      xml.div do |inner_frame|
+        filter.elements.inject("") do |memo, elt|
+          if options[:remote]
+            # AJAX style submission
+            submit_function = remote_function(remote_options.merge({:submit => options[:id]}))
+            xml << elt.to_html((options[:selects]||{}).merge(:onchange => submit_function))
+          else
+            # standard style submission
+            xml << elt.to_html
+          end
+        end
+      end
+      xml << "</form>"
+    end
+  end
+end
---
+++ lib/data_table/sort_view_helpers.rb
@@ -0,0 +1,43 @@
+module DataTable
+  module SortViewHelpers
+    
+    # TODO: *UGH* including filter_name *FAIL*
+    def sort_header(name = nil, options = {})
+      filter = find_filter(name).with(params)
+      filter.sort.with(params).options.each do |o| 
+        render_sort_option_to_html(o, options)
+      end.join(<<-CR
+      
+      CR
+      )
+    end
+    
+    def render_sort_option_to_html(s, options)
+      caption = caption_for(s)
+      sort_option_url = sort_option_url_for(s)
+      icon = icon_for(s)
+      # assemble the bits
+    end
+    
+    def sort_option_url_for(sort_option, options)
+      # for the url that we generate, we link to the *opposite* sort
+      "?sort_key=#{sort_option.key}&sort_order=#{sort_option.other_order}"
+    end
+
+    # examine fetched parameters and set up, determine appearance of sort arrow
+    def icon_for(sort_option, options)
+      icon = if sort_option.current_order
+        "sort_#{sort_option.current_order}"
+      else
+        "sortArrow001"
+      end    
+      image_tag("chrome/#{icon}.gif", :alt => "Sort by #{caption_for(sort_option, options)}")
+    end
+
+    # examine fetched parameters, options, and set up to determine linked caption
+    def caption_for(sort_option, options = {})
+      caption = options[:caption] || Inflector::humanize(sort_option.key).titleize
+    end
+    
+  end
+end
