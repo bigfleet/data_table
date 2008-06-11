aff4db6a86a24024f7e6ae34a1a61c83dc7316f7
Jim Van Fleet <jim@jimvanfleet.com>
2008-06-11 15:51:57 -0400

    New flagship specification file

---
+++ spec/data_table_spec.rb
@@ -0,0 +1,15 @@
+describe "a data_table" do
+  
+  it "should have optional sorting"
+  
+  it "should have optional filtering"
+  
+  it "should wrap pagination with will_paginate"
+  
+  it "should be able to store standard form submission parameters"
+  
+  it "should be able to store remote form submission parameters"
+  
+  it "should be able to generate a hash of all known form data, including full current state"
+  
+end
