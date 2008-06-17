081ae5c67d76c5e8dca7337703ca88bf754bf036
Jim Van Fleet <jim@jimvanfleet.com>
2008-06-17 11:43:13 -0400

    Pagination support in good shape

--- spec/pagination_support_spec.rb
+++ spec/pagination_support_spec.rb
@@ -79,30 +79,59 @@ describe DataTable::PaginationSupport do
     
   end
   
-  describe "with parameters for filtering" do
+  describe "with parameters for filtering" do   
     
+    before(:each) do
+      @params = {:cars => {:color => "blue"}}
+      @cars.params = @params
+    end
     
     describe "the standard url" do
-      it "should include the page and the filtering options"
+      it "should include only the page" do
+        @cars.url_for_pagination_params(2).should == 
+          {"cars[page]" => "2", "cars[color]" => "blue"}
+      end
     end
     
     describe "the remote url options" do
-      it "should include the page and the filtering options"
-      it "should respect the other appropriate remote options"
+      
+      before(:each) do
+        @cars.options = @opts
+      end
+      
+      it "should respect the other appropriate remote options" do
+        @cars.remote_pagination_options_for(2)[:update].should == "hotBox"
+        @cars.remote_pagination_options_for(2)[:method].should == "get"
+      end
     end
     
   end
   
   describe "with parameters for filtering and sorting" do
     
+    before(:each) do
+      @params = {:cars => {:color => "blue", :sort_key => "year", :sort_order => "desc"}}
+      @cars.params = @params
+    end
     
     describe "the standard url" do
-      it "should include the page and all appropriate options"
+      it "should include only the page" do
+        @cars.url_for_pagination_params(2).should == 
+          {"cars[color]"=>"blue", "cars[page]"=>"2", 
+            "cars[sort_key]"=>"year", "cars[sort_order]"=>"desc"}
+      end
     end
     
     describe "the remote url options" do
-      it "should include the page and all appropriate options"
-      it "should respect the other appropriate remote options"
+      
+      before(:each) do
+        @cars.options = @opts
+      end
+      
+      it "should respect the other appropriate remote options" do
+        @cars.remote_pagination_options_for(2)[:update].should == "hotBox"
+        @cars.remote_pagination_options_for(2)[:method].should == "get"
+      end
     end
     
   end
